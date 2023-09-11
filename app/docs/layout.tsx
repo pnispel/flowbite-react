ad5c74234c324f98774d46422a3e9e5fa100a798
Conner Davis <conner@tulup.io>
2023-09-10 22:56:03 -0700

    docs(app/docs/layout): add sidebar link to `<MegaMenu>` docs

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -265,6 +265,7 @@ const DocsSidebar: FC<DocsLayoutState> = ({ isCollapsed, setCollapsed }) => {
                     <SidebarLink href="/docs/components/footer">Footer</SidebarLink>
                     <SidebarLink href="/docs/components/forms">Forms</SidebarLink>
                     <SidebarLink href="/docs/components/list-group">List group</SidebarLink>
+                    <SidebarLink href="/docs/components/mega-menu">Mega menu</SidebarLink>
                     <SidebarLink href="/docs/components/modal">Modal</SidebarLink>
                     <SidebarLink href="/docs/components/navbar">Navbar</SidebarLink>
                     <SidebarLink href="/docs/components/pagination">Pagination</SidebarLink>
